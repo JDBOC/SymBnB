6963ede60f3df127c9d1e3051cbe0be95db96155
Jean Daniel Boccara <jdboccara@gmail.com>
2019-10-04 11:49:08 +0200

    rajout calendrier en JS

--- src/Form/BookingType.php
+++ src/Form/BookingType.php
@@ -3,21 +3,36 @@
   namespace App\Form;
 
   use App\Entity\Booking;
-  use Symfony\Component\Form\Extension\Core\Type\DateType;
+
+
+  use App\Form\DataTransformer\FrenchToDateTimeTransformer;
+  use DateTime;
   use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+  use Symfony\Component\Form\Extension\Core\Type\TextType;
   use Symfony\Component\Form\FormBuilderInterface;
+  use Symfony\Component\Form\FormTypeInterface;
   use Symfony\Component\OptionsResolver\OptionsResolver;
+
 
   class BookingType extends ApplicationType
   {
+    public function __construct(FrenchToDateTimeTransformer $transformer)
+    {
+      $this->transformer = $transformer;
+    }
+
+
     public function buildForm(FormBuilderInterface $builder , array $options)
     {
       $builder
-        ->add ( 'startDate' , DateType::class , $this->getConfig ( "Date d'arrivée" , "Votre date d'arrivée" , ["widget" => 'single_text'] ) )
-        ->add ( 'endDate' , DateType::class , $this->getConfig ( "date de départ" , "Votre date de départ" , ["widget" => 'single_text'] ) )
+        ->add ( 'startDate' , TextType::class , $this->getConfig ( "Date d'arrivée" , "Votre date d'arrivée") )
+        ->add ( 'endDate' , TextType::class , $this->getConfig ( "date de départ" , "Votre date de départ") )
         ->add ( 'comment' , TextareaType::class , $this->getConfig ( "Commentaire" , "laissez nous un commentaire" , [
           "required" => false
         ] ) );
+
+      $builder -> get ('startDate')->addModelTransformer ($this->transformer);
+      $builder ->get ('endDate')->addModelTransformer ($this->transformer);
     }
 
     public function configureOptions(OptionsResolver $resolver)
